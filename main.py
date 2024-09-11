73998ad2083edbcf717642e977800e73df3b6871
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-09-11 16:46:27 +0000

    create layout with kivy

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ class HeartRateApp(App):
     def build(self):
         layout = BoxLayout(orientation'vertical')
         self.heart_rate_label = Label(text='Fetching heart rate data...')
-        return Label(text='Welcome to POTS Heartrate Monitor!')
+        layout.add_widget(self.heart_rate_label)
+        # Direct user to Fitbit authorization URL on start
+        print("Please go to the following URL and authorize the application:")
+        print(get_authorization_url())
+        # Use Clock to periodically fetch heart rate data
+        Clock.schedule_interval(self.update_heart_rate_data, 60) # Update every 60 seconds
+        return layout
 
 if __name__ == '__main__':
     HeartRateApp().run()
